273c6a1cc4a531f13f51f0a512c4f4132e5bf8a8
KorirMoze <korirkipsmoze@gmail.com>
2025-05-13 11:05:19 +0300

    adding google out

--- ussd-sandbox/nuxt.config.ts
+++ ussd-sandbox/nuxt.config.ts
@@ -3,6 +3,5 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
-  // Remove any existing PostCSS configuration if you have it
-  // The module will handle all Tailwind setup automatically
+
 })
--- ussd-sandbox/pages/index.vue
+++ ussd-sandbox/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="p-6">
-      <MainPage />
+      <Login />
     </div>
   </template>
 <script  setup>
 import { ref, onMounted } from 'vue'
 import MainPage from "../pages/sandbox.vue";
+import Login from "../components/Login.vue";
 </script>
